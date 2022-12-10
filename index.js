7b8040511e2f95149b54c049cb15e3716c9e96dc
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-12-10 08:50:44 +0530

    Added some more dependencies and connected the index.html file

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express")
+const bodyParser = require("body-parser");
+
+
+
 
 const app = express()
 const port = 3000;
 
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+
 app.get("/",(req, res)=> {
-    res.send("Hello from Home Route")
+    res.sendFile(__dirname+"/index.html")
+})
+app.post("/",(req, res)=> {
+    console.log(req.body.todo)
+    res.redirect("/")
 })
 
 
