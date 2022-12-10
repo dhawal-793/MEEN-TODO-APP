eabdc3a438d38f0494005c881a4c7472c346856b
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-12-10 09:25:59 +0530

    Updated app.js to run on dynamic port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const todos=[]
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 const date = new Date();
 const options = {
     weekday: "long",
