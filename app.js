c3a9b34b804be55aeb0000fecf4d73ba258b8e60
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-12-10 10:06:50 +0530

    Created connection to mongodb database and modified list.ejs file

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // Imports
 const express = require("express")
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose")
 const date = require(__dirname + "/date")
-
+require("dotenv").config()
 
 // Constants
 const todos = []
 const app = express()
 const port = process.env.PORT || 3000;
 const today = date.getDay();
+const mongo_url = process.env.MONGO_URL
 
 // App Methods
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,13 +18,34 @@ app.use(express.static(__dirname + "/public"))
 app.set("view engine", "ejs")
 
 
+// Database
+
+mongoose.connect(mongo_url, { useNewUrlParser: true }, (err) => {
+    if (err) throw err;
+    console.log("DB Connected successfully");
+});
+
+const itemSchema = {
+    name: String
+}
+const Item = mongoose.model("Item", itemSchema);
+
+
 // Routes
 app.get("/", (req, res) => {
-    res.render("list", { "listTitle": today, "todos": todos });
+    Item.find({}, (err, items) => {
+        if (err) throw (err)
+
+        res.render("list", { listTitle: today, todos: items });
+    })
+    // res.render("list", { "listTitle": today, "todos": todos });
 })
 app.post("/", (req, res) => {
     const todo = req.body.todo;
-    todos.push(todo);
+    const item = new Item({
+        name: todo
+    })
+    item.save();
     res.redirect("/")
 })
 
