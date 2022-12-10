90f9b4ea96c2776035b54d44a9c4e4f374949a9d
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-12-10 09:17:20 +0530

    Added code to show current day as title

--- app.js
+++ app.js
@@ -6,14 +6,23 @@ const bodyParser = require("body-parser");
 const todos=[]
 const app = express()
 const port = 3000;
+const date = new Date();
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+}
+const today=date.toLocaleDateString("en-us",options)
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(__dirname+"/public"))
 app.set("view engine", "ejs")
 
+
+
 app.get("/",(req, res)=> {
-    res.render("list",{"listTitle":"ToDoList","todos":todos});
+    res.render("list",{"listTitle":today,"todos":todos});
 })
 app.post("/",(req, res)=> {
     const todo = req.body.todo;
