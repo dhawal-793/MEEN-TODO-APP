5321dbf953886eaae79a1e63b26ea01cbb2d806c
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-12-10 09:45:26 +0530

    Added Comments and completed version-v1 of the app

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
+// Imports
 const express = require("express")
 const bodyParser = require("body-parser");
-const date =require(__dirname+"/date")
+const date = require(__dirname + "/date")
 
 
-
-const todos=[]
+// Constants
+const todos = []
 const app = express()
 const port = process.env.PORT || 3000;
 const today = date.getDay();
 
-
+// App Methods
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(__dirname+"/public"))
+app.use(express.static(__dirname + "/public"))
 app.set("view engine", "ejs")
 
 
-
-app.get("/",(req, res)=> {
-    res.render("list",{"listTitle":today,"todos":todos});
+// Routes
+app.get("/", (req, res) => {
+    res.render("list", { "listTitle": today, "todos": todos });
 })
-app.post("/",(req, res)=> {
+app.post("/", (req, res) => {
     const todo = req.body.todo;
     todos.push(todo);
     res.redirect("/")
 })
+
 
 app.listen(port, () => {
     console.log(`Server is Running on Port ${port}`)
